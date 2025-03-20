3bdca18edcb62e906bfc4a48cc3745e5c2753efb
Joshua Bixler <jbixler@vervocity.io>
2025-03-19 23:39:19 -0500

    Fix setting last audio file path

--- src/EventListener/AudioFilePostFlushListener.php
+++ src/EventListener/AudioFilePostFlushListener.php
@@ -4,11 +4,16 @@ namespace App\EventListener;
 
 use App\Entity\AudioFile;
 use Doctrine\Bundle\DoctrineBundle\Attribute\AsEntityListener;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Events;
 
 #[AsEntityListener(event: Events::postPersist, entity: AudioFile::class, method: 'postPersist')]
 final class AudioFilePostFlushListener
 {
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+    ) {}
+    
     public function postPersist(AudioFile $audioFile): void
     {
         $number = str_pad($audioFile->getId(), 9, '0', STR_PAD_LEFT);
@@ -28,5 +33,6 @@ final class AudioFilePostFlushListener
         rename($audioFile->getPath(), $path);
         
         $audioFile->setPath($path);
+        $this->entityManager->flush();
     }
 }
